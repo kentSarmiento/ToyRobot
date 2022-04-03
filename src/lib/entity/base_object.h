d1dfaa54aa07b3585ccb33113bd709306a5083b5
Kent Tristan Yves Sarmiento <kentsarmiento@gmail.com>
2022-04-03 22:22:53 +0800

    Implementation and refactoring for Table operations

--- src/lib/entity/base_object.h
+++ src/lib/entity/base_object.h
@@ -1,8 +1,12 @@
 #ifndef ENTITY_BASE_OBJECT_H_
 #define ENTITY_BASE_OBJECT_H_
 
+#include <string>
+
 #include "object_type.h"
 #include "coordinates.h"
+
+using namespace std;
 
 namespace toyrobot {
 
@@ -25,23 +29,33 @@ public:
         id_ = 0;
     }
 
-    BaseObject(int id, Coordinates position, ObjectType type)
-        : position_(position),
-          type_(type) {
-        id_ = id;
-    }
-
     BaseObject(const BaseObject &o) {
         id_ = o.id_;
         position_ = o.position_;
         type_ = o.type_;
     }
 
+    virtual ~BaseObject() = default;
+
+    virtual Coordinates GetMovePosition() { return position_; }
+    virtual void Move() {}
+
+    virtual void TurnLeft() {}
+    virtual void TurnRight() {}
+
+    virtual string Report() { return {}; }
+
+    bool ComparePosition(Coordinates position);
+
 protected:
     int id_;
     Coordinates position_;
     ObjectType type_;
 };
+
+inline bool BaseObject::ComparePosition(Coordinates position) {
+    return (position_.x == position.x && position_.y == position.y);
+}
 
 } // namespace toyrobot
 
